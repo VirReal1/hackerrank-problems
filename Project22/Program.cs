f2a8a3f7f49f788e9f9e27ad238ef2b489a83f63
VirReal <cangokceaslan@outlook.com>
2021-03-14 16:57:47 +0300

    Project22 Added

---
+++ Project22/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Counting Valleys Program
+    public static int countingValleys(int steps, string path)
+    {
+        int pathWay = 0, numberOfValleys = 0;
+
+        for (int i = 0; i < steps; i++)
+        {
+            if (path[i] == 'D')
+            {
+                pathWay++;
+            }
+
+            else if(path[i] == 'U')
+            {
+                pathWay--;
+            }
+
+            if (pathWay == 0 && path[i] == 'U')
+            {
+                numberOfValleys++;
+            }
+        }
+        return numberOfValleys;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
