e8169ed59b262dfef2501ae10f7a52618a0e3bc9
VirReal <cangokceaslan@outlook.com>
2021-04-29 21:59:13 +0300

    Project28 Added

---
+++ Project28/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.ComponentModel.Design;
+using System.Xml.Schema;
+
+class Result
+{
+
+    // Forming a Magic Square Problem
+    public static int formingMagicSquare(List<List<int>> s)
+    {
+        int changes = 0;
+
+        int min = 100;
+
+        int[,,] all =
+        {
+            {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+            {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+            {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+            {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+            {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+            {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+            {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+            {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}
+        };
+
+        for (int i = 0; i < 8; i++)
+        {
+            changes = 0;
+            for (int y = 0; y < 3; y++)
+            {
+                for (int z = 0; z < 3; z++)
+                {
+                    changes = changes + Math.Abs(all[i, y, z]-s[y][z]);
+                }
+            }
+
+            if (changes < min)
+            {
+                min = changes;
+            }
+        }
+        return min;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<List<int>> s = new List<List<int>>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            s.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList());
+        }
+
+        int result = Result.formingMagicSquare(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
