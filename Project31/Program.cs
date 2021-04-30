2979184b55c4f969bc82b28011370279ac021179
VirReal <cangokceaslan@outlook.com>
2021-04-30 20:52:39 +0300

    Project31 Added

---
+++ Project31/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Angry Professor Problem
+    public static string angryProfessor(int k, List<int> a)
+    {
+        int earlyStudents = 0;
+
+        for (int i = 0; i < a.Count; i++)
+        {
+            if (a[i] <= 0)
+            {
+                earlyStudents++;
+            }
+        }
+
+        if (earlyStudents >= k)
+        {
+            return "NO";
+        }
+        else
+        {
+            return "YES";
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            string result = Result.angryProfessor(k, a);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
