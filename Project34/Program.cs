5654e62618903b15249332b34548627da3da294b
VirReal <cangokceaslan@outlook.com>
2021-05-03 03:18:13 +0300

    Project34 Added

---
+++ Project34/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Save the Prisoner! Problem
+    public static int saveThePrisoner(int n, int m, int s) //prisoner--candy-start chair
+    {
+        s = s + m - 1;
+        s = s % n;
+        if (s == 0)
+        {
+            s = n;
+        }
+        return s;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int s = Convert.ToInt32(firstMultipleInput[2]);
+
+            int result = Result.saveThePrisoner(n, m, s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
