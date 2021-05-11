f8a497aee50e5fa84d501a01138153e3cd41b527
VirReal <cangokceaslan@outlook.com>
2021-05-11 19:54:03 +0300

    Project38 Added

---
+++ Project38/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Find Digits Problem
+    public static int findDigits(int n)
+    {
+        int count = 0;
+        string number = n.ToString();
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (n%char.GetNumericValue(number[i])==0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.findDigits(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
