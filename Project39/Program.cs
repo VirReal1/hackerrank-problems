7205e709f8ed9d7ec5f2a2ab3cad4cdda7c2aadd
VirReal <cangokceaslan@outlook.com>
2021-05-11 20:15:50 +0300

    Project39 Added

---
+++ Project39/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    // Extra Long Factorials Problem
+    public static void extraLongFactorials(int n)
+    {
+        BigInteger number = 1;
+        for (BigInteger i = 2; i <= n; i++)
+        {
+            number = number * i;
+        }
+        Console.WriteLine(number);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.extraLongFactorials(n);
+    }
+}
