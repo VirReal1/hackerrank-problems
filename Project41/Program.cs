05fe1d2106b56429f98e7d9fdd1bc421b14e4202
VirReal <cangokceaslan@outlook.com>
2021-05-13 22:13:22 +0300

    Project41 Added

---
+++ Project41/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Sherlock and Squares Problem
+    public static int squares(int a, int b)
+    {
+        int count = 0;
+        double x = Math.Sqrt(a);
+        x = Math.Round(x,0);
+        double y = Math.Sqrt(b);
+        y = Math.Round(y,0);
+        for (double i = x; i >= x && i <= y; i++)
+        {
+            if ((i * i) >= a && (i * i <= b))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(firstMultipleInput[0]);
+
+            int b = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.squares(a, b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
