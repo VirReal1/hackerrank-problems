00b5cea6dbef0afcacbb9510a4b63c3801666dbd
VirReal <cangokceaslan@outlook.com>
2021-05-24 17:41:21 +0300

    Project44 Added

---
+++ Project44/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Repeated String Problem
+    public static long repeatedString(string s, long n)
+    {
+        long count = 0;
+
+        long extra = (n % s.Length);
+
+        count = ((n - extra)/s.Length) * s.Count(f => (f == 'a'));
+
+        for (int i = 0; i < extra; i++)
+        {
+            if (s[i] == 'a')
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+        long result = Result.repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
