412012496b1607e7579595a9566d3062bd8e81a4
VirReal <cangokceaslan@outlook.com>
2021-10-26 15:48:42 +0300

    Project57 Added

---
+++ Project57/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    // Halloween Sale Problem
+    public static int howManyGames(int p, int d, int m, int s)
+    {
+        int cost = 0;
+        int numberOfGames = 0;
+
+        do
+        {
+            cost += p;
+            p -= d;
+            if (p <= m)
+                p = m;
+            if (cost <= s)
+                numberOfGames++;
+
+        } while (cost <= s);
+
+        return numberOfGames;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int p = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        int m = Convert.ToInt32(firstMultipleInput[2]);
+
+        int s = Convert.ToInt32(firstMultipleInput[3]);
+
+        int answer = Result.howManyGames(p, d, m, s);
+
+        textWriter.WriteLine(answer);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
